93c1ff8e3c178196deda62ad2bbee1759465d3a7
jinyus <jinyhehe@gmail.com>
2024-05-21 11:45:31 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/elice/ggorangjirang/GgorangjirangApplication.java
@@ -0,0 +1,13 @@
+package com.elice.ggorangjirang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgorangjirangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgorangjirangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/ggorangjirang/GgorangjirangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.ggorangjirang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgorangjirangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
