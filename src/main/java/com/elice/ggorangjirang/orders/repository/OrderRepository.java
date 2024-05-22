f8c9da18046a131b749646381e81bd868a23ddd1
Jong-Min <codejomo99@gmail.com>
2024-05-22 22:36:24 +0900

    feat : OrderItem, Order Repository 추가

--- src/main/java/com/elice/ggorangjirang/orders/repository/OrderRepository.java
+++ src/main/java/com/elice/ggorangjirang/orders/repository/OrderRepository.java
@@ -1,5 +1,8 @@
 package com.elice.ggorangjirang.orders.repository;
 
-public class OrderRepository {
+import com.elice.ggorangjirang.orders.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
 
 }
