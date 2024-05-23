7e542b6db71c478e1c0329fbd2f3c8740e3549e2
Jong-Min <codejomo99@gmail.com>
2024-05-23 22:49:44 +0900

    fix : 어노테이션 수정

--- src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
+++ src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.elice.ggorangjirang.orders.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,8 @@ public class OrderController {
 
   private final OrderService orderService;
 
-  @PostMapping("")
-  public ResponseEntity<String> saveOrder(OrderSaveRequest request){
+  @PostMapping
+  public ResponseEntity<String> saveOrder(@RequestBody OrderSaveRequest request){
     orderService.saveOrder(request);
 
     return ResponseEntity.ok("주문 성공");
