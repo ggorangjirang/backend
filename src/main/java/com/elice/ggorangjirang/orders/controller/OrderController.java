5dac99b9d12da5476d52e5f5d033e0b5d75ae780
Jong-Min <codejomo99@gmail.com>
2024-05-23 12:19:23 +0900

    Refactor : 파일명 변경

--- src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
+++ src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
-public class OrdersController {
+public class OrderController {
 
   private final OrderService orderService;
 
