3c97dd6b52702696380837d52794d9467766d975
Jiwon <jiwon5568@gmail.com>
2024-05-23 20:55:43 +0900

    Fix: 자체 회원가입 기능 구현중

--- src/main/java/com/elice/ggorangjirang/users/service/UserService.java
+++ src/main/java/com/elice/ggorangjirang/users/service/UserService.java
@@ -1,6 +1,8 @@
 package com.elice.ggorangjirang.users.service;
 
 import com.elice.ggorangjirang.users.dto.UserSignupDto;
+import com.elice.ggorangjirang.users.entity.Role;
+import com.elice.ggorangjirang.users.entity.Users;
 import com.elice.ggorangjirang.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,12 +17,22 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
+
     public void signUp(UserSignupDto userSignupDto) throws Exception {
         if (userRepository.findByEmail(userSignupDto.getEmail()).isPresent()) {
             throw new Exception("이미 존재하는 이메일입니다.");
         }
 
+        Users users = Users.builder()
+            .email(userSignupDto.getEmail())
+            .password(userSignupDto.getPassword())
+            .name(userSignupDto.getName())
+            .role(Role.USER)
+            .build();
 
+        users.passwordEncode(passwordEncoder);
+        userRepository.save(users);
     }
+
 
 }
