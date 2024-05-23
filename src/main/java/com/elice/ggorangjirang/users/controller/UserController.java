ad8d0b7691ed4747da221e8382171991c32137c4
Jiwon <jiwon5568@gmail.com>
2024-05-23 20:56:49 +0900

    Fix: 유저 컨트롤러 작성중

--- src/main/java/com/elice/ggorangjirang/users/controller/UserController.java
+++ src/main/java/com/elice/ggorangjirang/users/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.elice.ggorangjirang.users.controller;
 
-import org.springframework.stereotype.Controller;
+import com.elice.ggorangjirang.users.dto.UserSignupDto;
+import com.elice.ggorangjirang.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
 
-    
+    @PostMapping("/signup")
+    public String signUp(@RequestBody UserSignupDto userSignupDto) throws Exception {
+        userService.signUp(userSignupDto);
+
+        return "회원가입이 완료되었습니다.";
+    }
+
 }
