eaf072a806b501518487adb39f8adc837fddc22a
Jong-Min <codejomo99@gmail.com>
2024-05-24 01:23:34 +0900

    refactor

--- src/main/java/com/elice/ggorangjirang/orders/entity/OrderItem.java
+++ src/main/java/com/elice/ggorangjirang/orders/entity/OrderItem.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "order_item")
 public class OrderItem {
 
   @Id
