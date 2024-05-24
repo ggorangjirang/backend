f38d51de9c5fd7804695114f88a6a006636d8c9b
Jong-Min <codejomo99@gmail.com>
2024-05-25 00:22:08 +0900

    remove

--- src/main/java/com/elice/ggorangjirang/orders/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elice.ggorangjirang.orders.repository;
-
-import com.elice.ggorangjirang.orders.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-}
--- src/main/java/com/elice/ggorangjirang/orders/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elice.ggorangjirang.orders.repository;
-
-import com.elice.ggorangjirang.orders.entity.Order;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-  List<Order> findByUserId(Long userId);
-}
