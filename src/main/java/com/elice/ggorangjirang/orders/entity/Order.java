0fe1aed5b7e4bbc2585be5ce7de568373c2e5bf4
Jong-Min <codejomo99@gmail.com>
2024-05-25 00:28:56 +0900

    feat : Entity 추가

---
+++ src/main/java/com/elice/ggorangjirang/orders/entity/Order.java
@@ -0,0 +1,67 @@
+package com.elice.ggorangjirang.orders.entity;
+
+import com.elice.ggorangjirang.deliveries.entity.Deliveries;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "order_id")
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "deliveries_id")
+  private Deliveries deliveries;
+
+  private LocalDateTime orderDate;
+
+  @Enumerated(EnumType.STRING)
+  private OrderStatus orderStatus;
+
+  // User 매핑
+  private Long userId;
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  //양방향
+  public void addOrderItem(OrderItem orderItem){
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
+
+  public void setDeliveries(Deliveries deliveries){
+    this.deliveries = deliveries;
+    if (deliveries != null) {
+      deliveries.setOrder(this);
+    }
+  }
+
+
+
+}
