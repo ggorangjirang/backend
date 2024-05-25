d99b282374d82e4f2cd4575b8d44bd39c6c08097
wjcho <wj930303@gmail.com>
2024-05-25 19:30:23 +0900

    Feat: category and subcategory REST api functions.

---
+++ src/main/java/com/elice/ggorangjirang/categories/dto/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.elice.ggorangjirang.categories.dto;
+
+import com.elice.ggorangjirang.categories.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+    private String categoryName;
+}
---
+++ src/main/java/com/elice/ggorangjirang/subcategories/controller/SubcategoryController.java
@@ -0,0 +1,22 @@
+package com.elice.ggorangjirang.subcategories.controller;
+
+import com.elice.ggorangjirang.subcategories.dto.SubcategoryResponse;
+import com.elice.ggorangjirang.subcategories.service.SubcategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SubcategoryController {
+    private final SubcategoryService subcategoryService;
+
+    @GetMapping("/api/subcategories")
+    public ResponseEntity<List<SubcategoryResponse>> getAllSubcategoriesWithCategoryName() {
+        List<SubcategoryResponse> subcategories = subcategoryService.getSubcategoriesWithCategoryName();
+        return ResponseEntity.ok(subcategories);
+    }
+}
