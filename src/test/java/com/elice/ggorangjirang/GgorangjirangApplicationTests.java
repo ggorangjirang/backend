02f14680abc14af9555844d641b446557da746d4
jinyus <jinyhehe@gmail.com>
2024-05-26 00:43:32 +0900

    fix: 환경변수 읽을 수 있도록 추가

--- src/test/java/com/elice/ggorangjirang/GgorangjirangApplicationTests.java
+++ src/test/java/com/elice/ggorangjirang/GgorangjirangApplicationTests.java
@@ -1,9 +1,14 @@
 package com.elice.ggorangjirang;
 
+import com.elice.ggorangjirang.global.config.EnvConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
+@PropertySource(value = {
+		"classpath:env.yml",
+}, factory = EnvConfig.class)
 class GgorangjirangApplicationTests {
 
 	@Test
