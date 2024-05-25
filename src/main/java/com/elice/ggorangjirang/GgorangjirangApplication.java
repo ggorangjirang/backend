edd7128ca249f38a6791c3a90b1c52e90b01849e
jinyus <jinyhehe@gmail.com>
2024-05-26 00:06:27 +0900

    feat: 설정파일 env.yml 읽어오도록 추가 및 mysql db 의존성 추가

--- src/main/java/com/elice/ggorangjirang/GgorangjirangApplication.java
+++ src/main/java/com/elice/ggorangjirang/GgorangjirangApplication.java
@@ -1,11 +1,16 @@
 package com.elice.ggorangjirang;
 
+import com.elice.ggorangjirang.global.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource(value = {
+		"classpath:env.yml",
+}, factory = EnvConfig.class)
 public class GgorangjirangApplication {
 
 	public static void main(String[] args) {
