c31299ed75499002a02464062df4707d809b0844
Jiwon <jiwon5568@gmail.com>
2024-05-26 21:43:38 +0900

    Fix: 로그인 커스텀 필터 추가

---
+++ src/main/java/com/elice/ggorangjirang/global/login/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.elice.ggorangjirang.global.login.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+// "/login" 요청 왔을 때 JSON 값을 매핑 처리하는 필터
+public class CustomUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private static final String DEFAULT_LOGIN_REQUEST_URL = "/login"; // "/login"으로 오는 요청을 처리
+    private static final String HTTP_METHOD = "POST"; // 로그인 http 메소드는 POST
+    private static final String CONTENT_TYPE = "application/json"; // json 타입의 데이터로 오는 로그인 요청만 처리
+    // json 값에 email, password
+    private static final String USERNAME_KEY = "email";
+    private static final String PASSWORD_KEY = "password";
+    private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_MATCHER
+        = new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL, HTTP_METHOD);
+
+    private final ObjectMapper objectMapper;
+
+    public CustomUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper) {
+        super(DEFAULT_LOGIN_PATH_MATCHER);
+        this.objectMapper = objectMapper;
+    }
+
+    /* 인증 처리 메소드
+       요청 JSON example
+       {
+            "email": "elice1234@elice.com",
+            "password": "test1234"
+        }
+    */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
+        if (request.getContentType() == null || !request.getContentType().equals(CONTENT_TYPE)) {
+            throw new AuthenticationServiceException("Authentication Content-Type not supported: " + request.getContentType());
+        }
+
+        String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+        Map<String, String> usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
+
+        String email = usernamePasswordMap.get(USERNAME_KEY);
+        String password = usernamePasswordMap.get(PASSWORD_KEY);
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(email, password);
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+}
