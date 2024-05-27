448e6e91cb36676441d570ddeb8a19a8d0ab84c9
Jong-Min <codejomo99@gmail.com>
2024-05-27 16:55:58 +0900

    feat : 구매 여부

--- src/main/java/com/elice/ggorangjirang/orders/repository/OrderItemRepository.java
+++ src/main/java/com/elice/ggorangjirang/orders/repository/OrderItemRepository.java
@@ -2,7 +2,10 @@ package com.elice.ggorangjirang.orders.repository;
 
 import com.elice.ggorangjirang.orders.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
+  @Query("SELECT COUNT(oi) > 0 FROM OrderItem oi WHERE oi.order.users.id = :userId AND oi.product.id = :productId")
+  boolean existsByUserIdAndProductId(@Param("userId") Long userId, @Param("productId") Long productId);
 }
