c1f818a14b2bae4c6128330677cdc3d8dfa79fdd
Jiwon <jiwon5568@gmail.com>
2024-05-27 22:57:31 +0900

    Feat: Security Config 초기 세팅

---
+++ src/main/java/com/elice/ggorangjirang/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.elice.ggorangjirang.global.config;
+
+import com.elice.ggorangjirang.global.login.service.LoginService;
+import com.elice.ggorangjirang.jwt.service.JwtService;
+import com.elice.ggorangjirang.users.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final LoginService loginService;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            
+
+            .cors();
+
+            // OAuth2.0 구현 후 추가 예정
+
+            return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
