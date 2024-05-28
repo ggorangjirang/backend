fafe13ec8056ebd4f7f56155e0a24dce8fa1b8f4
Jong-Min <codejomo99@gmail.com>
2024-05-28 17:28:03 +0900

    refactor : api/ 추가

--- src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
+++ src/main/java/com/elice/ggorangjirang/orders/controller/OrderController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/orders")
+@RequestMapping("api/orders")
 public class OrderController {
   private final OrderService orderService;
   private final OrderItemService orderItemService;
--- src/main/java/com/elice/ggorangjirang/orders/controller/OrderItemController.java
+++ src/main/java/com/elice/ggorangjirang/orders/controller/OrderItemController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/orderitem")
+@RequestMapping("api/orderitem")
 public class OrderItemController {
   private final ProductService productService;
   private final OrderItemService orderItemService;
