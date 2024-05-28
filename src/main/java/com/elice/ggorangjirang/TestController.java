cc5ae1044de2740a35ee846771f7afccd7ea08e7
jinyus <jinyhehe@gmail.com>
2024-05-28 15:44:08 +0900

    fix: post controller

--- src/main/java/com/elice/ggorangjirang/TestController.java
+++ src/main/java/com/elice/ggorangjirang/TestController.java
@@ -1,6 +1,7 @@
 package com.elice.ggorangjirang;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
