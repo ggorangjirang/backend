ef5ef7c3082a4c7c5f6a9dbe460853ac230d26ac
Jong-Min <codejomo99@gmail.com>
2024-05-29 17:08:09 +0900

    feat : 비동기적 처리

--- src/main/java/com/elice/ggorangjirang/orders/controller/OrderAdminController.java
+++ src/main/java/com/elice/ggorangjirang/orders/controller/OrderAdminController.java
@@ -2,17 +2,22 @@ package com.elice.ggorangjirang.orders.controller;
 
 
 
+import com.elice.ggorangjirang.deliveries.dto.DeliveryStatusDto;
 import com.elice.ggorangjirang.deliveries.service.DeliveryService;
 import com.elice.ggorangjirang.orders.dto.OrderResponse;
 import com.elice.ggorangjirang.orders.service.OrderItemService;
 import com.elice.ggorangjirang.orders.service.OrderService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -40,15 +45,13 @@ public class OrderAdminController {
 
   // 회원 주문 취소
   @DeleteMapping("/{id}")
-  public String deleteUsersOrder(@PathVariable(value = "id") Long id, Model model) {
+  public ResponseEntity<?> deleteUsersOrder(@PathVariable(value = "id") Long id) {
     try {
-      orderService.cancel(id);
-      return "redirect:admin-orders/admin-orders"; // 삭제 후 목록 페이지로 리디렉션
+      orderService.deleteById(id);
+      return ResponseEntity.ok().build();
     } catch (IllegalStateException e) {
-      model.addAttribute("error", e.getMessage());
-      return "error"; // 에러 페이지 템플릿을 반환
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
   }
-
 
 }
