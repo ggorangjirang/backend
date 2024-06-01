5e5be98da599a671abc555a063b5adc1b95e1970
Jiwon <jiwon5568@gmail.com>
2024-06-02 00:04:38 +0900

    Fix: id로 유저 찾는 메소드 추가

--- src/main/java/com/elice/ggorangjirang/users/repository/UserRepository.java
+++ src/main/java/com/elice/ggorangjirang/users/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.elice.ggorangjirang.users.repository;
 
 import com.elice.ggorangjirang.users.entity.Users;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
+
+    Optional<Users> findById(Long id);
 
     Optional<Users> findByRefreshToken(String refreshToken);
 
