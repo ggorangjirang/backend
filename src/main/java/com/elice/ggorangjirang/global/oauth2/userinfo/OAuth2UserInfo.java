2a3a96507019e2ed6824029ac72cdceef6b83c3a
Jiwon <jiwon5568@gmail.com>
2024-06-02 00:03:54 +0900

    Fix: OAuth2 유저 정보 클래스 추가

---
+++ src/main/java/com/elice/ggorangjirang/global/oauth2/userinfo/OAuth2UserInfo.java
@@ -0,0 +1,30 @@
+package com.elice.ggorangjirang.global.oauth2.userinfo;
+
+import java.util.Map;
+
+public class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getId() {
+        return String.valueOf(attributes.get("id"));
+    }
+    public String getName() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+
+        if (account == null) {
+            return null;
+        }
+
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        if (profile == null) {
+            return null;
+        }
+
+        return (String) profile.get("nickname");
+    }
+}
