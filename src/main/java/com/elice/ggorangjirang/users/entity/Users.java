552099a3a27d0837afed34e1118e38657192c789
Jiwon <jiwon5568@gmail.com>
2024-06-02 00:04:17 +0900

    Feat: @Bean 어노테이션 삭제

--- src/main/java/com/elice/ggorangjirang/users/entity/Users.java
+++ src/main/java/com/elice/ggorangjirang/users/entity/Users.java
@@ -71,9 +71,9 @@ public class Users {
     public void authorizeUsers() {
         this.role = Role.USER;
     }
+
     // 비밀번호 암호화 메소드
-    @Bean
-    public void passwordEncode(PasswordEncoder passwordEncoder) {
+    public void passwordEncoder(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
     }
 
