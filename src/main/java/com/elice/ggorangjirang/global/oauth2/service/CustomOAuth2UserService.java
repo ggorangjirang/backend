084eb63446f0290e194bc65a75737d8cc1237dd2
Jiwon <jiwon5568@gmail.com>
2024-06-02 23:26:12 +0900

    Style: 메소드 이름 변경(save -> add)

--- src/main/java/com/elice/ggorangjirang/global/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/elice/ggorangjirang/global/oauth2/service/CustomOAuth2UserService.java
@@ -53,19 +53,19 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     /*
      * attributes에 들어있는 소셜 로그인의 식별값 id를 통해 회원을 찾아 반환하는 메소드
-     * 만약 찾은 회원이 있다면, 그대로 반환하고 없다면 saveUser()를 호출하여 회원을 저장
+     * 만약 찾은 회원이 있다면, 그대로 반환하고 없다면 addUser()를 호출하여 회원을 저장
      */
     private Users getUser(OAuthAttributes attributes) {
         Users findUser = userRepository.findBySocialId(attributes.getOAuth2UserInfo().getId()).orElse(null);
 
         if (findUser == null) {
-            return saveUser(attributes);
+            return addUser(attributes);
         }
         return findUser;
     }
 
     // 생성된 User 객체를 DB에 저장 : socialId, email, role 값만 있는 상태
-    private Users saveUser(OAuthAttributes attributes) {
+    private Users addUser(OAuthAttributes attributes) {
         Users createdUser = attributes.toEntity(attributes.getOAuth2UserInfo());
         return userRepository.save(createdUser);
     }
