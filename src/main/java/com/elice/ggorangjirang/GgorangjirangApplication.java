259fedb57de4a3e1af2aa2679dfd47f9b0d56d2b
jinyus <jinyhehe@gmail.com>
2024-06-03 02:09:12 +0900

    fix: spring doc cors 설정

--- src/main/java/com/elice/ggorangjirang/GgorangjirangApplication.java
+++ src/main/java/com/elice/ggorangjirang/GgorangjirangApplication.java
@@ -1,6 +1,8 @@
 package com.elice.ggorangjirang;
 
 import com.elice.ggorangjirang.global.config.EnvConfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -11,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @PropertySource(value = {
 		"classpath:env.yml",
 }, factory = EnvConfig.class)
+@OpenAPIDefinition(servers = {@Server(url = "https://ggorangjirang.duckdns.org", description = "GRJR")})
 public class GgorangjirangApplication {
 
 	public static void main(String[] args) {
