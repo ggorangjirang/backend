77d5b4adf79d6549ea9ef2faa51bd9e46e689a29
Jiwon <jiwon5568@gmail.com>
2024-06-02 23:27:20 +0900

    Fix: 로그 레벨 상승

--- src/main/java/com/elice/ggorangjirang/global/oauth2/handler/OAuth2LoginFailHandler.java
+++ src/main/java/com/elice/ggorangjirang/global/oauth2/handler/OAuth2LoginFailHandler.java
@@ -21,6 +21,6 @@ public class OAuth2LoginFailHandler implements AuthenticationFailureHandler {
                                         AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 ERROR
         response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
-        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+        log.warn("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
     }
 }
