42520341e0a3d3c41732fedaee8de8161af7d06c
Jiwon <jiwon5568@gmail.com>
2024-06-04 21:56:19 +0900

    Feat: 회원가입 시 카트 자동 생성 추가

--- src/main/java/com/elice/ggorangjirang/users/service/UserService.java
+++ src/main/java/com/elice/ggorangjirang/users/service/UserService.java
@@ -1,5 +1,6 @@
 package com.elice.ggorangjirang.users.service;
 
+import com.elice.ggorangjirang.carts.service.CartService;
 import com.elice.ggorangjirang.users.dto.UserSignupDto;
 import com.elice.ggorangjirang.users.entity.Role;
 import com.elice.ggorangjirang.users.entity.Users;
@@ -16,6 +17,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
+    private final CartService cartService;
 
 
     public void signUp(UserSignupDto userSignupDto) throws Exception {
@@ -32,6 +34,8 @@ public class UserService {
 
         users.passwordEncode(passwordEncoder);
         userRepository.save(users);
+
+        cartService.createCartForUser(users);
     }
 
     public Users findByUsername(String username) {
