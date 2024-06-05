c61adea7cfb36fabd6818597c0128faecf25ce1a
Jiwon <jiwon5568@gmail.com>
2024-06-05 18:54:45 +0900

    Fix: 로그인 기능 정상 작동

--- src/main/java/com/elice/ggorangjirang/global/login/service/LoginService.java
+++ src/main/java/com/elice/ggorangjirang/global/login/service/LoginService.java
@@ -1,15 +1,19 @@
 package com.elice.ggorangjirang.global.login.service;
 
+import com.elice.ggorangjirang.jwt.service.JwtService;
 import com.elice.ggorangjirang.jwt.util.CustomUserDetails;
 import com.elice.ggorangjirang.users.dto.UserLoginDto;
 import com.elice.ggorangjirang.users.entity.Users;
 import com.elice.ggorangjirang.users.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class LoginService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -26,10 +31,21 @@ public class LoginService implements UserDetailsService {
         return new CustomUserDetails(users);
     }
 
-    public boolean login(UserLoginDto userLoginDto) {
+    public String login(@RequestBody UserLoginDto userLoginDto, HttpServletResponse response) {
         var user = userRepository.findByEmail(userLoginDto.getEmail())
             .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
-        return passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword());
+
+        if (passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
+            String accessToken = jwtService.createAccessToken(user.getEmail());
+            String refreshToken = jwtService.createRefreshToken();
+
+            user.updateRefreshToken(refreshToken);
+            userRepository.saveAndFlush(user);
+            jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+            return accessToken;
+        } else {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
     }
 
 }
