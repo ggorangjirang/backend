f8093fa5a31728fbe0280658be6a55911e4b7d1d
jinyus <jinyhehe@gmail.com>
2024-06-08 04:34:40 +0900

    feat: 시큐리티 설정 변경

--- src/main/java/com/elice/ggorangjirang/global/config/SecurityConfig.java
+++ src/main/java/com/elice/ggorangjirang/global/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import com.elice.ggorangjirang.jwt.filter.JwtAuthenticationProcessingFilter;
 import com.elice.ggorangjirang.jwt.service.JwtService;
 import com.elice.ggorangjirang.users.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,98 +47,112 @@ public class SecurityConfig {
 
     private final OAuth2LoginFailHandler oAuth2LoginFailHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-            .cors(withDefaults())
-            .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/h2-console/**",
-                                "/swagger-ui/**",
-                                "/swagger-resources/**",
-                                "/api/**",
-                                "/api/login/oauth2/**",
-                                "/v3/api-docs/**",
-                                "/admin/**",
-                                "/js/**",
-                                "/css/**",
-                                "/actuator/**")
+    http.cors(
+        (cors) ->
+            cors.configurationSource(
+                request -> {
+                  CorsConfiguration config = new CorsConfiguration();
+
+                  config.setAllowedOrigins(
+                      List.of("http://localhost:3000", "https://ggorangjirang.duckdns.org"));
+                  config.setAllowedMethods(Collections.singletonList("*"));
+                  config.setAllowCredentials(true);
+                  config.setAllowedHeaders(Collections.singletonList("*"));
+                  config.setExposedHeaders(Collections.singletonList("Authorization"));
+                  config.setExposedHeaders(Collections.singletonList("Refresh-Token"));
+                  return config;
+                }));
+
+    http.cors(withDefaults())
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers(
+                        "/h2-console/**",
+                        "/swagger-ui/**",
+                        "/swagger-resources/**",
+                        "/api/**",
+                        "/api/login/oauth2/**",
+                        "/v3/api-docs/**",
+                        "/admin/**",
+                        "/js/**",
+                        "/css/**",
+                        "/actuator/**")
                     .permitAll()
-                    .anyRequest().authenticated()
-                );
-        http
-            .formLogin(config -> config.disable())
-            .httpBasic(config -> config.disable())
-            .csrf(config -> config.disable())
-            .headers(config -> config.disable())
+                    .anyRequest()
+                    .authenticated());
+    http.formLogin(config -> config.disable())
+        .httpBasic(config -> config.disable())
+        .csrf(config -> config.disable())
+        .headers(config -> config.disable())
+        .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-            .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http
-            .oauth2Login(oauth2Login -> oauth2Login
+    http.oauth2Login(
+        oauth2Login ->
+            oauth2Login
                 .successHandler(oAuth2LoginSuccessHandler)
                 .failureHandler(oAuth2LoginFailHandler)
-                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService))
-            );
+                .userInfoEndpoint(
+                    userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService)));
 
-        http
-            .addFilterAfter(customUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
-            .addFilterBefore(jwtAuthenticationProcessingFilter(), CustomUsernamePasswordAuthenticationFilter.class);
+    http.addFilterAfter(customUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+        .addFilterBefore(
+            jwtAuthenticationProcessingFilter(), CustomUsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+    return http.build();
+  }
 
-    // cors() 설정을 별도의 bean으로 관리
-    @Bean
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "https://ggorangjirang.duckdns.org"
-        ));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(List.of("x-auth-token"));
-        configuration.setAllowCredentials(true); // 쿠키와 인증 정보를 포함한 요청이 가능하도록 설정
+  // cors() 설정을 별도의 bean으로 관리
+//  @Bean
+//  public CorsConfigurationSource configurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(
+//        List.of("http://localhost:3000", "https://ggorangjirang.duckdns.org"));
+//    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//    configuration.setAllowedHeaders(List.of("authorization", "content-type", "x-auth-token"));
+//    configuration.setExposedHeaders(List.of("x-auth-token"));
+//    configuration.setAllowCredentials(true); // 쿠키와 인증 정보를 포함한 요청이 가능하도록 설정
+//
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder);
+    provider.setUserDetailsService(loginService);
+    return new ProviderManager(provider);
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(loginService);
-        return new ProviderManager(provider);
-    }
+  @Bean
+  public LoginSuccessHandler loginSuccessHandler() {
+    return new LoginSuccessHandler(jwtService, userRepository);
+  }
 
-    @Bean
-    public LoginSuccessHandler loginSuccessHandler() {
-        return new LoginSuccessHandler(jwtService, userRepository);
-    }
+  @Bean
+  public LoginFailHandler loginFailHandler() {
+    return new LoginFailHandler();
+  }
 
-    @Bean
-    public LoginFailHandler loginFailHandler() {
-        return new LoginFailHandler();
-    }
+  @Bean
+  public CustomUsernamePasswordAuthenticationFilter customUsernamePasswordAuthenticationFilter() {
+    CustomUsernamePasswordAuthenticationFilter filter =
+        new CustomUsernamePasswordAuthenticationFilter(objectMapper);
+    filter.setAuthenticationManager(authenticationManager());
+    filter.setAuthenticationSuccessHandler(loginSuccessHandler());
+    filter.setAuthenticationFailureHandler(loginFailHandler());
+    return filter;
+  }
 
-    @Bean
-    public CustomUsernamePasswordAuthenticationFilter customUsernamePasswordAuthenticationFilter() {
-        CustomUsernamePasswordAuthenticationFilter filter
-            = new CustomUsernamePasswordAuthenticationFilter(objectMapper);
-        filter.setAuthenticationManager(authenticationManager());
-        filter.setAuthenticationSuccessHandler(loginSuccessHandler());
-        filter.setAuthenticationFailureHandler(loginFailHandler());
-        return filter;
-    }
-
-    @Bean
-    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-        JwtAuthenticationProcessingFilter filter = new JwtAuthenticationProcessingFilter(jwtService, userRepository);
-        return filter;
-    }
-
+  @Bean
+  public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+    JwtAuthenticationProcessingFilter filter =
+        new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+    return filter;
+  }
 }
-
