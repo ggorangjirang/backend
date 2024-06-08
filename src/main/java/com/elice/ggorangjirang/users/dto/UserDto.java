bfcb379d77892ae80b6ae378d5e6bea6bfc15b6b
Jiwon <jiwon5568@gmail.com>
2024-06-08 22:17:21 +0900

    Feat: 유저 정보 DTO 추가

--- src/main/java/com/elice/ggorangjirang/users/dto/UserDto.java
+++ src/main/java/com/elice/ggorangjirang/users/dto/UserDto.java
@@ -1,2 +1,14 @@
-package com.elice.ggorangjirang.users.dto;public class UserDto {
+package com.elice.ggorangjirang.users.dto;
+
+import com.elice.ggorangjirang.users.entity.Address;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Address address;
 }
