f8d999097331514b0050116794cf6f35290849ff
Jiwon <jiwon5568@gmail.com>
2024-06-09 21:37:04 +0900

    Feat: 유저 정보 변경 dto 추가

---
+++ src/main/java/com/elice/ggorangjirang/users/dto/UserUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.elice.ggorangjirang.users.dto;
+
+import com.elice.ggorangjirang.users.entity.Address;
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String name;
+    private String phoneNumber;
+    private String currentPassword; // 기존 비밀번호
+    private String newPassword; // 새 배밀번호
+    private String confirmPassword; // 비밀번호 확인
+    private Address address;
+}
