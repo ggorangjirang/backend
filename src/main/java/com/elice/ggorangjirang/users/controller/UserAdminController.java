ad4586eec7d534fcbb243388b4c93d612c73e0bc
Jiwon <jiwon5568@gmail.com>
2024-06-10 22:01:49 +0900

    Feat: 유저 어드민 컨트롤러 추가

---
+++ src/main/java/com/elice/ggorangjirang/users/controller/UserAdminController.java
@@ -0,0 +1,26 @@
+package com.elice.ggorangjirang.users.controller;
+
+import com.elice.ggorangjirang.users.dto.UserDto;
+import com.elice.ggorangjirang.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/users")
+public class UserAdminController {
+    private final UserService userService;
+
+    @GetMapping
+    public String getAllUsers(Model model) {
+        List<UserDto> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "admin-users/admin-users";
+    }
+}
