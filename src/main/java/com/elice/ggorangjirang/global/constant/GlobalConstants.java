199300ac76b269fd3931b70d85a69cbeb85597ca
wjcho <wj930303@gmail.com>
2024-06-11 09:35:05 +0900

    Chore: 공백 제거

--- src/main/java/com/elice/ggorangjirang/global/constant/GlobalConstants.java
+++ src/main/java/com/elice/ggorangjirang/global/constant/GlobalConstants.java
@@ -10,7 +10,6 @@ public class GlobalConstants {
     public static final String NEW_PRODUCT_NOTICE = "New Product has created: ";
     public static final String PRODUCT_CHANGED_NOTICE = "Product has changed. Check the details in logs: ";
     public static final String PRODUCT_DELETED_NOTICE = "Product has deleted: ";
-
     public static final String PRODUCT_STOCK_NOTICE = " has less than 5 items left in stock.";
     public static final String DELIVERY_COMPLETE_EMAIL_TITLE = "[꼬랑지랑] 주문하신 상품이 배송 완료되었습니다. ";
     public static final String DELIVERY_COMPLETE_EMAIL_CONTENT= "주문하신 상품의 배송이 완료되었습니다. \n주문번호: ";
