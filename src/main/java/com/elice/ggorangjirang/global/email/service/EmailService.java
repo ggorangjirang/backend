2a54a3485d19293f7ceac8ef8337c58a49895c43
Jiwon <jiwon5568@gmail.com>
2024-06-12 15:54:23 +0900

    Feat: 이메일 인증 발송 메소드 추가

--- src/main/java/com/elice/ggorangjirang/global/email/service/EmailService.java
+++ src/main/java/com/elice/ggorangjirang/global/email/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.elice.ggorangjirang.global.email.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +24,18 @@ public class EmailService {
         message.setTo(receiverEmail);
         message.setSubject(title);
         message.setText(content);
+
+        mailSender.send(message);
+    }
+
+    public void sendHtmlMessage(String receiverEmail, String title, String htmlBody) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom(fromAddress);
+        helper.setTo(receiverEmail);
+        helper.setSubject(title);
+        helper.setText(htmlBody, true);
 
         mailSender.send(message);
     }
