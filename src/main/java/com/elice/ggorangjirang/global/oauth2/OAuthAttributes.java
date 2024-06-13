c1508e2ea2d6a1d899d8ac05e38813dc2214bf6a
Jiwon <jiwon5568@gmail.com>
2024-06-14 05:55:31 +0900

    Fix: OAuth2 관련 부분 수정

--- src/main/java/com/elice/ggorangjirang/global/oauth2/OAuthAttributes.java
+++ src/main/java/com/elice/ggorangjirang/global/oauth2/OAuthAttributes.java
@@ -14,21 +14,29 @@ public class OAuthAttributes {
 
     private String nameAttributeKey; // OAuth2 로그인 진행 시 키가 되는 필드 값
     private OAuth2UserInfo oAuth2UserInfo;
+    private String email;
+    private String name;
 
     @Builder
-    private OAuthAttributes(String nameAttributeKey, OAuth2UserInfo oAuth2UserInfo) {
+    private OAuthAttributes(String nameAttributeKey, OAuth2UserInfo oAuth2UserInfo, String email, String name) {
         this.nameAttributeKey = nameAttributeKey;
         this.oAuth2UserInfo = oAuth2UserInfo;
+        this.email = email;
+        this.name = name;
     }
 
     public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        OAuth2UserInfo userInfo = new OAuth2UserInfo(attributes);
+
         return OAuthAttributes.builder()
             .nameAttributeKey(userNameAttributeName)
             .oAuth2UserInfo(new OAuth2UserInfo(attributes))
+            .email(userInfo.getEmail())
+            .name(userInfo.getName())
             .build();
     }
 
-    public Users toEntity(OAuth2UserInfo oAuth2UserInfo) {
+    public Users toEntity() {
         return Users.builder()
             .socialId(oAuth2UserInfo.getId())
             .email(oAuth2UserInfo.getEmail())
--- src/main/java/com/elice/ggorangjirang/global/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/elice/ggorangjirang/global/oauth2/service/CustomOAuth2UserService.java
@@ -2,12 +2,14 @@ package com.elice.ggorangjirang.global.oauth2.service;
 
 import com.elice.ggorangjirang.global.oauth2.CustomOAuth2User;
 import com.elice.ggorangjirang.global.oauth2.OAuthAttributes;
+import com.elice.ggorangjirang.global.oauth2.userinfo.OAuth2UserInfo;
 import com.elice.ggorangjirang.jwt.service.JwtService;
 import com.elice.ggorangjirang.users.entity.Role;
 import com.elice.ggorangjirang.users.entity.Users;
 import com.elice.ggorangjirang.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -15,6 +17,9 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.Map;
@@ -26,6 +31,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final RestTemplate restTemplate;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -72,13 +78,66 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     // 생성된 User 객체를 DB에 저장 : email, role 값만 있는 상태
     public Users addUser(OAuthAttributes attributes) {
-        Users createdUser = attributes.toEntity(attributes.getOAuth2UserInfo());
+        Users createdUser = attributes.toEntity();
         createdUser.setRole(Role.USER);
 
         String refreshToken = jwtService.createRefreshToken();
         createdUser.updateRefreshToken(refreshToken);
 
-
         return userRepository.save(createdUser);
+    }
+
+    public String getAccessToken(String authorizationCode) {
+        String tokenUri = "https://kauth.kakao.com/oauth/token";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "YOUR_KAKAO_CLIENT_ID"); // 실제 카카오 클라이언트 ID로 대체
+        params.add("redirect_uri", "YOUR_REDIRECT_URI"); // 실제 리다이렉트 URI로 대체
+        params.add("code", authorizationCode);
+        params.add("client_secret", "YOUR_CLIENT_SECRET"); // 클라이언트 시크릿이 필요한 경우 추가
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<Map> response = restTemplate.postForEntity(tokenUri, request, Map.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Map<String, Object> responseBody = response.getBody();
+            return (String) responseBody.get("access_token");
+        } else {
+            throw new RuntimeException("Failed to get access token from Kakao");
+        }
+    }
+
+    public OAuthAttributes getUserInfo(String accessToken) {
+        String userInfoUri = "https://kapi.kakao.com/v2/user/me";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(userInfoUri, HttpMethod.GET, request, Map.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Map<String, Object> responseBody = response.getBody();
+            Map<String, Object> kakaoAccount = (Map<String, Object>) responseBody.get("kakao_account");
+            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+            return OAuthAttributes.builder()
+                .nameAttributeKey("id")
+                .oAuth2UserInfo(new OAuth2UserInfo(responseBody))
+                .email((String) kakaoAccount.get("email"))
+                .name((String) profile.get("nickname"))
+                .build();
+        } else {
+            throw new RuntimeException("Failed to get user info from Kakao");
+        }
+    }
+
+    public Users addOrUpdateUser(OAuthAttributes attributes) {
+        Users user = userRepository.findByEmail(attributes.getEmail())
+            .map(entity -> entity.update(attributes.getName()))
+            .orElse(attributes.toEntity());
+        return userRepository.save(user);
     }
 }
