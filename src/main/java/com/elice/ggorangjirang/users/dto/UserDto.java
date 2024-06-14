57e3bdc1513f2c98910f6cd665d36aec04468e6c
Jiwon <jiwon5568@gmail.com>
2024-06-15 01:38:42 +0900

    Fix: 회원 정보 불러오기 위해 수정

--- src/main/java/com/elice/ggorangjirang/users/dto/UserDto.java
+++ src/main/java/com/elice/ggorangjirang/users/dto/UserDto.java
@@ -3,11 +3,19 @@ package com.elice.ggorangjirang.users.dto;
 import com.elice.ggorangjirang.users.entity.Address;
 import lombok.Data;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
 @Data
 public class UserDto {
     private Long id;
-    private String name;
     private String email;
+    private String name;
     private String phoneNumber;
-    private Address address;
+    private String address;
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
+    private String role;
 }
+
